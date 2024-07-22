fb389ccdc5d50683e8c54f77ca17ae5302b8e0cf
mray1111 <manishcode111@gmail.com>
2024-07-22 21:06:56 +0530

    Deployment ready

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ const mongoose=require('mongoose')
 const dotenv=require('dotenv')
 dotenv.config({path:"./config.env"})
 const mongooseURI=process.env.DB_URI
-console.log(mongooseURI)
+//console.log(mongooseURI)
+
 
 
 const connectToMongo=()=>{
